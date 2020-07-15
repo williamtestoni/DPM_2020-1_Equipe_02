3a57662a445e0c2884f808b32e8bdf327ad733a5
William Giovani Testoni <williamgiovane1@gmail.com>
2020-07-15 08:09:20 -0300

    [IMP] Portifolio Request

---
+++ Photoconnect/Photoconnect/Photoconnect/Classes/PortifolioRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Photoconnect.Classes
+{
+    class PortifolioRequest
+    {
+        [JsonProperty("event_type")]
+        public string EventType { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("album_id")]
+        public string AlbumId { get; set; }
+    }
+}
